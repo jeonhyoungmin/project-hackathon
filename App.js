614e06036c25b003ac6bb30ee0c93470130fb59b
polarisjyb <evia_a@naver.com>
2022-09-29 12:47:42 +0900

    테스트

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>git 테스트입니다.</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;

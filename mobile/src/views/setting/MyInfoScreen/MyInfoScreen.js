c4b1064b56bd837ba282a5f9f9ecd3c000a00d62
polarisjyb <evia_a@naver.com>
2022-10-07 17:50:50 +0900

    MyInfoScreen - 아이디, 이메일 확인 및 메모 추가

--- mobile/src/views/setting/MyInfoScreen/MyInfoScreen.js
+++ mobile/src/views/setting/MyInfoScreen/MyInfoScreen.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+/* 컴포넌트 */
+import CustomInput from '../../../components/CustomInput/CustomInput';
+
 
 const MyInfoScreen = () => {
+
+  const [userid, setUserid] = useState('');
+  const [email, setEmail] = useState('');
+  const [note, setNote] = useState('');
+
   return (
-    <View style={styles.root}>
-      <Text>MyInfo</Text>
+    <View style={styles.container}>
+      <View style={styles.myinfo_id_email}>
+
+        <Text style={styles.title_id}>아이디</Text>
+        <CustomInput
+          placeholder="아이디"
+          value={userid}
+          setValue={setUserid}
+          />
+
+        <Text style={styles.title_email}>이메일</Text>    
+        <CustomInput
+          placeholder="이메일"
+          value={email}
+          setValue={setEmail}
+          secureTextEntry={true}
+          />
+      </View>
+      <View  style={styles.myinfo_note}>
+        <Text style={styles.title_note}>메모</Text>
+        <CustomInput
+          placeholder="메모"
+          value={note}
+          setValue={setNote} />
+      </View>
     </View>
+    
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
+  container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent:'center',
+  },
+  myinfo_id_email: {
+    width: '100%',
+    padding: 20,
+
+  },
+  myinfo_note: {
+    width: '100%',
+  //   height: 500,
+    padding: 20,
   }
+  
 });
 
 export default MyInfoScreen;

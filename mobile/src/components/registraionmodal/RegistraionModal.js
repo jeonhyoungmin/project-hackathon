be26170b78ca3cef36f624fde9e04fbadbe3fd33
jeonhyoungmin <jhmin7509@gmail.com>
2022-10-12 16:12:44 +0900

    계정 등록 모달 창

--- mobile/src/components/registraionmodal/RegistraionModal.js
+++ mobile/src/components/registraionmodal/RegistraionModal.js
@@ -1,41 +1,125 @@
-import { View, Text, Alert, Modal, StyleSheet, Pressable } from 'react-native'
+import { View, Text, Alert, Modal, StyleSheet, Pressable, Button, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import { Transition } from 'react-native-reanimated';
 
-const RegistraionModal = () => {
+const RegistraionModal = ({visible}) => {
 
-  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(visible);
 
   return (
-      <Modal 
+    <Modal
       animationType='slide'
       transparent={true}
       visible={modalVisible}
+      onBackdropPress={() => setModalVisible(!modalVisible)}
       onRequestClose={() => {
-        setModalVisible(!modalVisible);
-      }}
-      >
-        <View style={styles.container}>
-          <View style={styles.modalStyle}>
-            <Text>registraionModal</Text>
+        setModalVisible(!modalVisible)
+    }} 
+    >
+      <View style={styles.container}>
+        <View style={styles.modalStyle}>
+          <View style={styles.centerContainer}>
+            {/* 상단: 아이디, 비밀번호, url 컨테이너 */}
+            <View style={styles.modalTop}>
+              <View style={styles.accountContainer}>
+                <TextInput></TextInput>
+                <TextInput></TextInput>
+              </View>
+              <View style={styles.urlContainer}>
+                <TextInput></TextInput>
+              </View>
+            </View>
+
+
+
+            {/* 중단: 메모, 즐겨찾기 컨테이너 */}
+            <View style={styles.modalMiddle}>
+
+            </View>
+
+
+
+            {/* 하단: 등록, 닫기 컨테이너 */}
+            <View style={styles.modalBottom}>
+              <View style={styles.registContainer}>
+                <Pressable >
+                  <Text>등록</Text>
+                </Pressable>
+              </View>
+              <View style={styles.closeContainer}>
+                <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                  <Text>닫기</Text>
+                </Pressable>
+              </View>
+            </View>
+
+
           </View>
         </View>
-      </Modal>
+      </View>
+    </Modal>
   )
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   modalStyle: {
-    backgroundColor: 'blue',
+    borderWidth: 1,
+    borderColor: "#333",
+    backgroundColor: 'white',
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    width: 300,
-    height: 200,
+    width: "70%",
+    height: "50%",
+  },
+  centerContainer: {
+    width: '95%',
+    height: '95%',
+    // backgroundColor: 'yellow',
+  },
+
+
+
+  modalTop: {
+    flex: 3,
+    backgroundColor: 'blue'
+  },
+  accountContainer: {
+
+  },
+  urlContainer: {
+
+  },
+
+
+
+  modalMiddle: {
+    flex: 2,
+    backgroundColor: 'green'
+  },
+
+
+
+
+  modalBottom: {
+    flex: 1,
+    // backgroundColor: 'pink',
+    flexDirection: 'row',
+  },
+  registContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 

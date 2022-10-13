34de8f855b39b05dd626e4f39b87014320da3219
WonDuJin <windyjan1@naver.com>
2022-10-13 13:24:54 +0900

    북마크&메인페이지 연결

--- mobile/src/components/main/SearchBar.js
+++ mobile/src/components/main/SearchBar.js
@@ -13,43 +13,39 @@ import {Modal} from 'react-native';
 
 const {width} = Dimensions.get('window');
 
-const SearchBar = () => {
-  const [visible, setVisible] = useState(false);
+const SearchBar = (search, setSearch) => {
+  // const [search, setSearch] = useState(false);
   const {keyword, onChangeText} = useContext(SearchContext);
-  const toggleModal = () => {
-    setVisible(!visible);
-  };
-  return (
-    <View>
-      {/* <Modal
-        animationType="slide"
-        transparent={true}
-        visible={visible}
-        style={styles.modal}
-        onDismiss={toggleModal}> */}
-      <TextInput
-        style={[styles.searchInput, styles.searchbarPosition]}
-        placeholder="검색어를 입력해 주세요"
-        value={keyword}
-        onChangeText={onChangeText}
-        autoFocus
 
-        // value={props.searchText}
-        // onChangeText={text => props.setSrearchText(text)}
-        // onSubmitEditing={props.onSubmit}
-      />
-      {/* </Modal> */}
-      <Pressable
-        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}
-        onPress={() => setVisible(false)}>
-        <Icon name="cancel" size={5} color="#9e9e9e" />
-      </Pressable>
-    </View>
+  return (
+    <Modal animationType="slide" transparent={true} visible={search}>
+      <View style={styles.container}>
+        <View style={styles.modal}>
+          {/* <TextInput
+          style={[styles.searchInput, styles.searchbarPosition]}
+          placeholder="검색어를 입력해 주세요"
+          value={keyword}
+          onChangeText={onChangeText}
+          autoFocus
+        /> */}
+          <Pressable
+            style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}
+            onPress={() => setSearch(!search)}>
+            <Icon name="cancel" size={5} color="#9e9e9e" />
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
   );
 };
 // 검색창에 쓰일 검색바
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   searchInput: {
     borderColor: 'black',
     borderWidth: 1,
@@ -69,17 +65,15 @@ const styles = StyleSheet.create({
     marginLeft: 8,
   },
   modal: {
-    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#333',
+    backgroundColor: 'white',
+    borderRadius: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    height: '50%',
     position: 'absolute',
-    top: '50%',
-    left: '50%',
-    elevation: 5,
-    transform: [{translateX: -(width * 0.4)}, {translateY: -90}],
-    height: 180,
-    width: width * 0.8,
-    backgroundColor: '#fff',
-    borderRadius: 7,
   },
 });
 // 검색바에 쓰이는 스타일 값

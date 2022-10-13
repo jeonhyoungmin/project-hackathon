b16f82a51278ef1780d88e1986ddc03a8015f79f
jeonhyoungmin <jhmin7509@gmail.com>
2022-10-13 12:59:45 +0900

    modal textinput 컴포넌트

---
+++ mobile/src/components/modaltextinput/ModalTextInput.js
@@ -0,0 +1,27 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import React from 'react'
+
+const ModalTextInput = ({bgColor, placeholderText, containerWidth, containerHeight, textWidth, textHeight, pw}) => {
+  return (
+    <View style={[styles.container, bgColor && {backgroundColor: bgColor}, containerWidth && {width: containerWidth}, containerHeight && {height: containerHeight}]}>
+      <TextInput style={[styles.textInput, textWidth && {width: textWidth}, textHeight && {height: textHeight}]} placeholder={placeholderText} secureTextEntry={pw}/>
+    </View>
+  )
+}
+
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "95%",
+    // height: "95%",
+  },
+  textInput: {
+    // width: "100%",
+    // height: "80%",
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5
+  }
+})
+
+export default ModalTextInput

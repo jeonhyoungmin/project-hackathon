ab3d498c52ca4b32f634e3cfd1c09f95262d9e06
jeonhyoungmin <jhmin7509@gmail.com>
2022-10-18 13:05:14 +0900

    서비스 이름란 추가

--- server/controllers/BookMark.js
+++ server/controllers/BookMark.js
@@ -6,13 +6,14 @@ const bookmark = (req, res, next) => {
   const id = req.body.id
   const password = req.body.password
   const url = req.body.url
+  const service = req.body.service
   const memo = req.body.memo
 
   // SQL query문 작성
   // 매개 변수 1: 쿼리문
   // 매개 변수 2: VALUES의 ?에 들어갈 데이터 배열
   // 매개 변수 3: 콜백 함수(error, rows, fields)
-  connection.query('INSERT INTO bookmark (id, password, url, memo) VALUES (?,?,?,?)', [id, password, url, memo], function(err){
+  connection.query('INSERT INTO bookmark (regi_id, regi_password, regi_url, regi_service, regi_memo) VALUES (?,?,?,?,?)', [id, password, url, service, memo], function(err){
       if (err) throw err;
       res.send('입력 완료')
     });

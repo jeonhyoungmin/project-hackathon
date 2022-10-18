aa829440b966f511f4638717c55db1bff80ad52a
jeonhyoungmin <jhmin7509@gmail.com>
2022-10-18 19:53:43 +0900

    읽기, 삭제 라우터 추가

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import bookmark from '../controllers/BookMark.js'
+import { registrateaccount, registeredaccount, deleteaccount} from '../controllers/BookMark.js'
+
 const router = express.Router();
 
 // (매개변수1: bookmark 주소로 요청 발생 시, 매개변수2: bookmark controllers 실행)
-router.post('/bookmark', bookmark)
+
+// 북마크 서비스 계정 등록
+router.post('/bookmark', registrateaccount)
+// 북마크 등록된 서비스 계정 읽기
+router.get('/bookmark', registeredaccount)
+// 북마크 등록된 서비스 계정 삭제하기
+router.delete('/bookmark/:regi_id', deleteaccount)
 
 export default router;
